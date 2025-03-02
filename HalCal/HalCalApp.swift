e717921d09e4342386fb677b0bd61b7ea149ca10
Inderjeet Mander <inder1297@gmail.com>
2025-03-02 18:25:54 -0500

    Initial Commit

---
+++ HalCal/HalCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalCalApp.swift
+//  HalCal
+//
+//  Created by Inderjeet Mander on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct HalCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HalCalTests/HalCalTests.swift
@@ -0,0 +1,17 @@
+//
+//  HalCalTests.swift
+//  HalCalTests
+//
+//  Created by Inderjeet Mander on 3/2/25.
+//
+
+import Testing
+@testable import HalCal
+
+struct HalCalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
